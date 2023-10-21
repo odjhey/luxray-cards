15fff3c7f20437896f1435b189da3aa0568279ab
odjhey <odjhey@gmail.com>
2023-10-21 13:36:06 +0800

    feat: add swiper

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,5 +1,136 @@
 import { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
 import './App.css'
+
+const tmpData: {
+  kanji: string
+  furigana: string
+  romaji: string
+  en: string
+}[] = [
+  {
+    kanji: '人',
+    furigana: 'ひと',
+    romaji: 'hito',
+    en: 'person',
+  },
+  {
+    kanji: '大',
+    furigana: 'だい',
+    romaji: 'dai',
+    en: 'big',
+  },
+  {
+    kanji: '年',
+    furigana: 'ねん',
+    romaji: 'nen',
+    en: 'year',
+  },
+  {
+    kanji: '日',
+    furigana: 'ひ',
+    romaji: 'hi',
+    en: 'day',
+  },
+  {
+    kanji: '中',
+    furigana: 'ちゅう',
+    romaji: 'chuu',
+    en: 'middle',
+  },
+  {
+    kanji: '時',
+    furigana: 'じ',
+    romaji: 'ji',
+    en: 'time/hour',
+  },
+  {
+    kanji: '上',
+    furigana: 'うえ',
+    romaji: 'ue',
+    en: 'up/above',
+  },
+  {
+    kanji: '下',
+    furigana: 'した',
+    romaji: 'shita',
+    en: 'down/below',
+  },
+  {
+    kanji: '見',
+    furigana: 'みる',
+    romaji: 'miru',
+    en: 'see',
+  },
+  {
+    kanji: '口',
+    furigana: 'くち',
+    romaji: 'kuchi',
+    en: 'mouth',
+  },
+  {
+    kanji: '山',
+    furigana: 'やま',
+    romaji: 'yama',
+    en: 'mountain',
+  },
+  {
+    kanji: '出',
+    furigana: 'でる',
+    romaji: 'deru',
+    en: 'exit',
+  },
+  {
+    kanji: '手',
+    furigana: 'て',
+    romaji: 'te',
+    en: 'hand',
+  },
+  {
+    kanji: '目',
+    furigana: 'め',
+    romaji: 'me',
+    en: 'eye',
+  },
+  {
+    kanji: '田',
+    furigana: 'た',
+    romaji: 'ta',
+    en: 'rice field',
+  },
+  {
+    kanji: '前',
+    furigana: 'まえ',
+    romaji: 'mae',
+    en: 'before',
+  },
+  {
+    kanji: '生',
+    furigana: 'うむ',
+    romaji: 'umu',
+    en: 'birth/life',
+  },
+  {
+    kanji: '子',
+    furigana: 'こ',
+    romaji: 'ko',
+    en: 'child',
+  },
+  {
+    kanji: '女',
+    furigana: 'おんな',
+    romaji: 'onna',
+    en: 'woman',
+  },
+  {
+    kanji: '男',
+    furigana: 'おとこ',
+    romaji: 'otoko',
+    en: 'man',
+  },
+]
 
 function App() {
   return (
@@ -73,17 +204,23 @@ const FlashCard = () => {
         </div>
       </div>
       <div className="h-full w-full">
-        <SingleCard
-          contents={{
-            content: '好き',
-            sub: 'すき',
-            help: 'suki',
-          }}
-          options={{
-            showSub: options.showSub,
-            showHelp: options.showHelp,
-          }}
-        />
+        <Swiper className="h-full w-full">
+          {tmpData.map((v) => (
+            <SwiperSlide key={v.kanji}>
+              <SingleCard
+                contents={{
+                  content: v.kanji,
+                  sub: v.furigana,
+                  help: `${v.romaji} - ${v.en}`,
+                }}
+                options={{
+                  showSub: options.showSub,
+                  showHelp: options.showHelp,
+                }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <div className="flex gap-2 p-2">
         <button className="btn btn-primary">action</button>
