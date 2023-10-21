5ec3ea421015789b29facd12c6bfa23db2fe5a46
odjhey <odjhey@gmail.com>
2023-10-21 12:22:44 +0800

    feat: add temp layout

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,7 +1,61 @@
+import { PropsWithChildren, useState } from 'react'
 import './App.css'
 
 function App() {
-  return <div className="text-lg">Trifold Cards here we come.</div>
+  return (
+    <>
+      <div className="h-screen w-screen">
+        <FlashCard></FlashCard>
+      </div>
+    </>
+  )
+}
+
+const CardEntry = (props: PropsWithChildren) => {
+  return (
+    <div className="flex justify-center items-center border border-dashed border-secondary h-full w-full">
+      {props.children}
+    </div>
+  )
+}
+
+const DisplayText = (props: PropsWithChildren<{ show: boolean }>) => {
+  const [show, setShow] = useState(props.show)
+
+  return (
+    <div className="flex w-full justify-between p-4">
+      <div className={`flex justify-center items-center w-full`}>
+        <div className={`text-5xl ${show ? '' : 'hidden'}`}>
+          {props.children}
+        </div>
+      </div>
+      <button
+        className="btn"
+        type="button"
+        onClick={() => {
+          setShow((v) => !v)
+        }}
+      >
+        action
+      </button>
+    </div>
+  )
+}
+
+const FlashCard = () => {
+  return (
+    <div className="flex flex-col h-full w-full border border-solid border-primary justify-evenly items-center">
+      <CardEntry>
+        <DisplayText show={true}>好き</DisplayText>
+      </CardEntry>
+      <CardEntry>
+        <DisplayText show={false}>すき</DisplayText>
+      </CardEntry>
+      <CardEntry>
+        <DisplayText show={false}>suki</DisplayText>
+      </CardEntry>
+    </div>
+  )
 }
 
 export default App
