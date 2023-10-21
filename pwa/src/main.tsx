50f24abe4e9594d6c2e96135a66b6903856a08d8
odjhey <odjhey@gmail.com>
2023-10-21 11:59:59 +0800

    chore: convert to pwa

--- pwa/src/main.tsx
+++ pwa/src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+// const updateSW = registerSW({
+registerSW({
+  onOfflineReady() {
+    // test: do nothing for now
+  },
+})
--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        description: 'Flash Cards | houndlings.com',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'luxray-cards-logo-180x180.png',
+            sizes: '180x180',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'luxray-cards-logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'luxray-cards-logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+    react(),
+  ],
 })
